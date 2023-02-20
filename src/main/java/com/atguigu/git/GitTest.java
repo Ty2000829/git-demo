84d81cab6c526b14552e748ea09387b955f370b2
Leo <1736935315@qq.com>
2023-02-20 09:36:15 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author rpstart
+ * @create 2023-02-20 9:32
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
